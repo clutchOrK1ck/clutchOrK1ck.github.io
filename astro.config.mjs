8d0a94cb301750992f8006c096644caf194b16fd
clutchOrKick <serhii.poklonskyi@gmail.com>
2026-02-02 15:22:53 +0100

    setup webcoreui for the astro project
    configuring the webcore is required for the project to work correctly. ran in terminal: npm create webcore config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,1 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import react from "@astrojs/react";
-import markdoc from "@astrojs/markdoc";
-import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-
-  vite: {
-    plugins: [tailwindcss()],
-    optimizeDeps: {
-      include: ["@keystatic/core", "@keystatic/astro"],
-    },
-  },
-
-  output: "server",
-
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-});
+import { webcore } from "webcoreui/integration";// @ts-checkimport { defineConfig } from "astro/config";import tailwindcss from "@tailwindcss/vite";import react from "@astrojs/react";import markdoc from "@astrojs/markdoc";import keystatic from "@keystatic/astro";import vercel from "@astrojs/vercel";// https://astro.build/configexport default defineConfig({  integrations: [react(), markdoc(), keystatic(), webcore()],  vite: {    plugins: [tailwindcss()],    optimizeDeps: {      include: ["@keystatic/core", "@keystatic/astro"],    },  },  output: "server",  adapter: vercel({    webAnalytics: {      enabled: true,    },  }),});
